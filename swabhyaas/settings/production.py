8b2b481d257fc4b2a4ec3e822e87e4a3b16e85e8
sourabh27 <sourabh@chat360.io>
2023-11-22 08:58:55 +0530

    add support for docker

--- swabhyaas/settings/production.py
+++ swabhyaas/settings/production.py
@@ -8,6 +8,17 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS += ["anymail","storages",]
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
+    }
+}
+
 MIDDLEWARE += ['rollbar.contrib.django.middleware.RollbarNotifierMiddleware',]
 
 AUTH_PASSWORD_VALIDATORS = [
