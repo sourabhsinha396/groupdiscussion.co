0a017d67cd998ce648c1a765ccfef434ead16dfb
sourabh27 <sourabh@chat360.io>
2023-11-25 22:05:01 +0530

    add contrib.sites

--- swabhyaas/settings/base.py
+++ swabhyaas/settings/base.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
+    'django.contrib.sites',
 
     # our app
     "apps.common",
@@ -121,6 +122,8 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SITE_ID = 1
 
 AUTHENTICATION_BACKENDS = [
     'apps.authentication.backends.EmailOrUsernameModelBackend',
