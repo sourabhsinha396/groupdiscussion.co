a4d36f0d571babb3bb12629ed3cbc74a9016bb63
sourabh27 <sourabh@chat360.io>
2023-12-08 13:27:06 +0530

    increase referer site max length to avoid right truncation error

--- apps/analytics/models.py
+++ apps/analytics/models.py
@@ -6,7 +6,7 @@ class Hit(models.Model):
     ip_address = models.GenericIPAddressField(help_text="IP Address", blank=True, null=True)
     utm_source = models.CharField(max_length=100, help_text="UTM Source", blank=True, null=True)
     utm_medium = models.CharField(max_length=100, help_text="UTM Medium", blank=True, null=True)
-    referer_site = models.CharField(max_length=100, help_text="Referrer Website URL", blank=True, null=True)
+    referer_site = models.CharField(max_length=255, help_text="Referrer Website URL", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
